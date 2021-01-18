d9f0dc3b2e9ae666309868d4987abfd45ad3760b
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-18 23:58:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,8 +33,7 @@ def sorted_matrix
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
   
-  sorted_alphabetically = [
-    ]
+  sorted_alphabetically = produce_storage_room.sort()
   
 end
 
